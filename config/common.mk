61280b5b66b95dd988c5c86a0c737a5516c74bc7
Lucchetto <lucchetto.tie@live.com>
2019-05-09 14:08:00 +0530

    prebuilt: Smart Charging (4/4)
    Also includes:
    
      From: DennySPB <dennyspb@gmail.com>
      Date: Sat, 18 May 2019 02:18:35 +0530
      Subject: [PATCH] SmartCharging: Rework and add more customizations
    
      From: Joshua Primero <jprimero155@gmail.com>
      Date: Sat, 4 Apr 2020 23:00:46 -0700
      Subject: [PATCH] init: smart charging: chown batt slate mode
    
      * Most Samsung Devices have this node for resuming and stopping the charging..
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config/common.mk
+++ config/common.mk
@@ -78,6 +78,7 @@ PRODUCT_COPY_FILES += \
 
 # Lineage-specific init rc file
 PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/init/init.custom-system_ext.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.custom-system_ext.rc \
     vendor/lineage/prebuilt/common/etc/init/init.lineage-system_ext.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.lineage-system_ext.rc
 
 # Enable Android Beam on all targets
