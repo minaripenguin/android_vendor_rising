c783b53ab019db5231ffc5b3ee523ab1887570f5
Danny Lin <danny@kdrag0n.dev>
2022-09-24 06:40:07 +0800

    vendor: Import font modifications from protonAOSP
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config/common.mk
+++ config/common.mk
@@ -1,5 +1,6 @@
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)
+$(call inherit-product-if-exists, vendor/lineage/fonts/fonts.mk)
 
 PRODUCT_BRAND ?= LineageOS
 
