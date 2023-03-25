5b4695985b86bcd64e5ff312187b8e76fcfc0823
minaripenguin <minaripenguin@users.noreply.github.com>
2023-03-25 15:58:25 +0800

    audio: Import sound effects pack
    * taken from protonAOSP and rice A12
    
    Co-authored-by: kdrag0n <danny@kdrag0n.dev>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config/common.mk
+++ config/common.mk
@@ -1,6 +1,7 @@
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)
 $(call inherit-product-if-exists, vendor/lineage/fonts/fonts.mk)
+$(call inherit-product-if-exists, vendor/lineage/audio/audio.mk)
 
 PRODUCT_BRAND ?= LineageOS
 
