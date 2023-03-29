44b8efdf9b828cdb4820c42a393f34b0f7dcfde0
minaripenguin <minaripenguin@users.noreply.github.com>
2023-03-29 11:01:17 +0800

    config: Ship with minimal gapps by default
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config/common.mk
+++ config/common.mk
@@ -6,6 +6,8 @@ $(call inherit-product-if-exists, vendor/lineage/audio/audio.mk)
 
 -include vendor/gms/products/gms.mk
 
+TARGET_CORE_GMS ?= true
+
 PRODUCT_BRAND ?= LineageOS
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
