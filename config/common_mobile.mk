b9595da90ed715cda099218b9c56130a56d4eac5
minaripenguin <minaripenguin@users.noreply.github.com>
2023-03-30 20:19:34 +0800

    configs: Drop settings overlays
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -52,27 +52,21 @@ endif
 PRODUCT_PACKAGES += \
     IconPackCircularAndroidOverlay \
     IconPackCircularLauncherOverlay \
-    IconPackCircularSettingsOverlay \
     IconPackCircularSystemUIOverlay \
     IconPackFilledAndroidOverlay \
     IconPackFilledLauncherOverlay \
-    IconPackFilledSettingsOverlay \
     IconPackFilledSystemUIOverlay \
     IconPackKaiAndroidOverlay \
     IconPackKaiLauncherOverlay \
-    IconPackKaiSettingsOverlay \
     IconPackKaiSystemUIOverlay \
     IconPackRoundedAndroidOverlay \
     IconPackRoundedLauncherOverlay \
-    IconPackRoundedSettingsOverlay \
     IconPackRoundedSystemUIOverlay \
     IconPackSamAndroidOverlay \
     IconPackSamLauncherOverlay \
-    IconPackSamSettingsOverlay \
     IconPackSamSystemUIOverlay \
     IconPackVictorAndroidOverlay \
     IconPackVictorLauncherOverlay \
-    IconPackVictorSettingsOverlay \
     IconPackVictorSystemUIOverlay \
     IconShapePebbleOverlay \
     IconShapeRoundedRectOverlay \
