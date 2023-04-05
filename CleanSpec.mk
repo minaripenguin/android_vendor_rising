64203f5f0a7351878ab360da7145dc7070ba648d
minaripenguin <minaripenguin@users.noreply.github.com>
2023-04-05 13:28:37 +0800

    CleanSpec: Cleanup wipe kernel objects
    Change-Id: I8245f2de66837314b866bbcbfe7b236e8e4191a9
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -1,2 +1,2 @@
-$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/kernel)
-$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/KERNEL_OBJ)
+# Wipe all KERNEL_OBJ folders including other devices kernel objects
+$(call add-clean-step, rm -rf $(wildcard out/target/product/*/obj/KERNEL_OBJ))
