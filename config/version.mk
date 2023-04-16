870958983681526f97291a6ff4c42fb67590dea8
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2023-04-16 09:34:45 +0800

    config: Add build type flags for RisingOS
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config/version.mk
+++ config/version.mk
@@ -7,10 +7,40 @@ RISING_CODENAME := Atlantis
 RISING_CODE := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)
 
 CURRENT_DEVICE=$(shell echo "$(TARGET_PRODUCT)" | cut -d'_' -f 2,3)
+MAINTAINER_LIST = $(shell cat vendor/risingOTA/risingOS.maintainers)
+DEVICE_LIST = $(shell cat vendor/risingOTA/risingOS.devices)
+
+ifeq ($(filter $(CURRENT_DEVICE), $(DEVICE_LIST)), $(CURRENT_DEVICE))
+   ifeq ($(filter $(RISING_MAINTAINER), $(MAINTAINER_LIST)), $(RISING_MAINTAINER))
+      RISING_BUILDTYPE := OFFICIAL
+  else 
+     # the builder is overriding official flag on purpose
+     ifeq ($(RISING_BUILDTYPE), OFFICIAL)
+       $(error **********************************************************)
+       $(error *     A violation has been detected, aborting build      *)
+       $(error **********************************************************)
+     else 
+       $(warning **********************************************************************)
+       $(warning *   There is already an official maintainer for $(CURRENT_DEVICE)    *)
+       $(warning *              Setting build type to UNOFFICIAL                      *)
+       $(warning *    Please contact current official maintainer before distributing  *)
+       $(warning *              the current build to the community.                   *)
+       $(warning **********************************************************************)
+       RISING_BUILDTYPE := UNOFFICIAL
+     endif
+  endif
+else
+   ifeq ($(RISING_BUILDTYPE), OFFICIAL)
+     $(error **********************************************************)
+     $(error *     A violation has been detected, aborting build      *)
+     $(error **********************************************************)
+   endif
+  RISING_BUILDTYPE := COMMUNITY
+endif
 
 LINEAGE_VERSION_APPEND_TIME_OF_DAY ?= true
 ifeq ($(LINEAGE_VERSION_APPEND_TIME_OF_DAY),true)
-    LINEAGE_BUILD_DATE := $(shell date -u +%Y%m%d_%H%M%S)
+    LINEAGE_BUILD_DATE := $(shell date -u +%Y%m%d%H%M)
 else
     LINEAGE_BUILD_DATE := $(shell date -u +%Y%m%d)
 endif
