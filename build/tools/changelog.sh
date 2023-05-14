db4999169c9ac30daf2bcc46b70721469f19c5c4
minaripenguin <minaripenguin@users.noreply.github.com>
2023-05-14 20:27:20 +0800

    changelog.sh: Improvements and fixes
    Change-Id: I0d4e87263f8184de2c82412ae2d768eee2828d91
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- build/tools/changelog.sh
+++ build/tools/changelog.sh
@@ -15,52 +15,54 @@
 # limitations under the License.
 #
 
+# Define the variables
 Changelog=Changelog.txt
-
 DEVICE=$1
+changelog_days=${2:-10}
 
-if [ -f $Changelog ];
-then
-	rm -f $Changelog
+# Check if the changelog file exists, and if so, remove it
+if [ -f $Changelog ]; then
+    rm -f $Changelog
 fi
 
+# Create the changelog file
 touch $Changelog
 
-# define changelog_days using 'export changelog_days=10'
-# this can be done before intiate build environment (. build/envsetup.sh)
-if [ -z $changelog_days ];then
-	changelog_days=10
-else
-	if (($changelog_days > 30 )); then
-        echo "Changelog can not generated for more than 30 days. For how many days do you want to generate changelog again? (🕑 timeout 15 seconds - default to 10 days)"
-        read -r -t 15 changelog_days || changelog_days=10
-	fi
-fi
-
+# Get a list of all the repositories
 REPO_LIST="$(repo list --path | sed 's|^vendor/risingOTA$||')"
-for i in $(seq $changelog_days); do
+
+# Loop through the specified number of days
+for i in $(seq 1 $changelog_days); do
+
+    # Get the start and end dates
     After_Date=`date --date="$i days ago" +%m-%d-%Y`
     k=$(expr $i - 1)
     Until_Date=`date --date="$k days ago" +%m-%d-%Y`
 
-    # Line with after --- until was too long for a small ListView
-    echo '====================' >> $Changelog
-    echo  "     "$Until_Date    >> $Changelog
-    echo '====================' >> $Changelog
+    # Add a header to the changelog file
+    echo "==================== $Until_Date =====================" >> $Changelog
 
-    # Cycle through all available repos
+    # Loop through all the repositories
     for repo_path in $REPO_LIST; do
-        # Find commits between 2 dates
+
+        # Find commits between the two dates
         GIT_LOG="$(git -C "$repo_path" log --oneline --after="$After_Date" --until="$Until_Date")"
-        [ -n "$GIT_LOG" ] && {
+
+        # If there are any commits, add them to the changelog file
+        if [ -n "$GIT_LOG" ]; then
             printf '\n   * '; echo "$repo_path"
-            echo "$GIT_LOG"
-        } >> $Changelog
+            echo "$GIT_LOG" >> $Changelog
+        fi
     done
+
+    # Add a blank line to the changelog file
     echo >> $Changelog
 done
 
+# Fix the formatting of the changelog file
 sed -i 's/project/   */g' $Changelog
 
+# Copy the changelog file to the appropriate location
 cp $Changelog $OUT_DIR/target/product/$DEVICE/system/etc/
 mv $Changelog $OUT_DIR/target/product/$DEVICE/
+
