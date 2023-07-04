458ba7d4629b3016ce0f76904ef561d85fcf43b7
minaripenguin <minaripenguin@users.noreply.github.com>
2023-07-04 14:07:05 +0800

    config: Update pixel fingerprint to June ASB 2023
    Change-Id: I83ef4b8fec1d3fd2fb3a6aa43d31209f9fc231da
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config/rising.mk
+++ config/rising.mk
@@ -100,10 +100,10 @@ PRODUCT_PACKAGES += \
     Updater
 
 ifeq ($(TARGET_USE_PIXEL_FINGERPRINT), true)
-BUILD_FINGERPRINT := "google/cheetah/cheetah:13/TQ2A.230405.003.E1/9802792:user/release-keys"
+BUILD_FINGERPRINT := "google/cheetah/cheetah:13/TQ3A.230605.012/10204971:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="cheetah-user 13 TQ2A.230405.003.E1 9802792 release-keys"
+    PRIVATE_BUILD_DESC="cheetah-user 13 TQ3A.230605.012 10204971 release-keys"
 
 endif
 
