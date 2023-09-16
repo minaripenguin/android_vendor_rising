1fdd4c0e2fecb178cc7d184a92196e7652436592
Vinay Verma <vvinay@codeaurora.org>
2023-09-16 11:34:54 +0800

    config: Disable Async MTE on System Server
    Async mode MTE is enabled by default on System Server process.
    Add property arm64.memtag.process.system_server=off
    This property is read during system server forking process and
    accordingly Heap Tagging level is set.
    
    Change-Id: I8e1bc5505036dac8d436f263a1eee588aeaa78f4
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config/rising.mk
+++ config/rising.mk
@@ -54,6 +54,9 @@ endif
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.input.video_enabled=false
 
+PRODUCT_PRODUCT_PROPERTIES += \
+    arm64.memtag.process.system_server=off
+
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
