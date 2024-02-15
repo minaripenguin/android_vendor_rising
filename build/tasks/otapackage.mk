5311a8028f3ff1c1f4d9571d43bb5453c2b3d174
Alvin Francis <nivlafx@gmail.com>
2024-02-15 18:08:37 -0400

    createjson: Copy the generated json to our official OTA repo
    Change-Id: I991c93fc656e5d9fac0b8cb33c245d822fd6d87c
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>

--- build/tasks/otapackage.mk
+++ build/tasks/otapackage.mk
@@ -43,6 +43,7 @@ otapackage: $(INTERNAL_OTA_PACKAGE_TARGET)
 	@echo ""
 	@echo "Creating json OTA..." >&2
 	$(hide) ./vendor/rising/build/tools/createjson.sh $(TARGET_DEVICE) $(PRODUCT_OUT) rising-$(RISING_BUILD_VERSION)-ota.zip $(RISING_VERSION) $(RISING_CODENAME) $(RISING_PACKAGE_TYPE) $(RISING_RELEASE_TYPE)
+	$(hide) cp -f $(PRODUCT_OUT)/$(TARGET_DEVICE).json vendor/risingOTA/$(TARGET_DEVICE).json
 	@echo ":·.·.·::·.·.·::·.·.·::·.·.·::·.·.·::·.·.·::·.·.·::·.·.·::·.·.·:" >&2
 	@echo " Size            : $(shell du -hs $(RISING_TARGET_PACKAGE) | awk '{print $$1}')"
 	@echo " Size(in bytes)  : $(shell wc -c $(RISING_TARGET_PACKAGE) | awk '{print $$1}')"
